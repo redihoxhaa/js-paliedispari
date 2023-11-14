f1519fea6764864575024ea86717e59683ed6957
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-14 17:29:53 +0100

    feat: divisione codice in functions e operations

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,3 +1,18 @@
+// FUNCTIONS
+
+function generateANumber() {
+    const generatedNumber = Math.floor(Math.random() * 5) + 1;
+    return generatedNumber;
+}
+
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return true;
+    } return false;
+}
+
+// OPERATIONS
+
 const chosenTeam = prompt("Scrivi PARI o DISPARI in base a come vuoi giocare");
 const chosenNum = Number(prompt("Scegli un numero da 1 a 5, estremi inclusi"));
 const minNumber = 1;
@@ -14,23 +29,9 @@ if (chosenNum >= minNumber && chosenNum <= maxNumber) {
         console.log("Il pc ha scelto team PARI");
     }
 
-    function generateANumber() {
-        const generatedNumber = Math.floor(Math.random() * 5) + 1;
-        return generatedNumber;
-    }
-
     const pcNumber = generateANumber();
     console.log("Il pc ha generato " + pcNumber);
-
     const numberSum = chosenNum + pcNumber;
-
-    function evenOrOdd(number) {
-        if (number % 2 === 0) {
-            return true;
-        } return false;
-    }
-
-    const result = evenOrOdd(numberSum);
 
     if (chosenTeam === "PARI" && evenOrOdd(numberSum)) {
         console.log("Congratulazioni, hai vinto!")
